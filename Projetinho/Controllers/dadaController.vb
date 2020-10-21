ddafc8d8435e0bade3b47d04779bb9cd3631b3ee
user <user@DESKTOP-B640EA1>
2020-10-21 19:54:18 -0300

    dededede

---
+++ Projetinho/Controllers/dadaController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class dadaController
+        Inherits Controller
+
+        ' GET: dada
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
